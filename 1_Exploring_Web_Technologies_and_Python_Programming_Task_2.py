0ce662532d2c2f80ef098d2b561676fd3264b0e7
MArney86 <matthew.h.arney.1986@gmail.com>
2024-08-05 02:04:17 -0500

    corrections and commenting

--- 1_Exploring_Web_Technologies_and_Python_Programming_Task_2.py
+++ 1_Exploring_Web_Technologies_and_Python_Programming_Task_2.py
@@ -3,11 +3,15 @@ import json
 
 #Task 2
 def fetch_pokemon(pokemon_name):
+    #connect and grab response from Pokeapi
     url = (f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}')
     try:
         response = requests.get(url)
         pokemon_data = response.json()
+
+        #verify data recieved
         if isinstance(pokemon_data, dict):
+            #get info and display to user
             name = pokemon_data.get('name', 'No Name')
             abilities = pokemon_data.get('abilities', 'No abilities')
             abilities_out = abilities[0]['ability']['name']
@@ -15,6 +19,7 @@ def fetch_pokemon(pokemon_name):
                 abil_out = abilities_out + ', ' + abilities[i]['ability']['name']
             print(f"Name: {name}\nAbilities: {abil_out}")
 
+        #get status and reason for failure if faild response
         else: 
             status = pokemon_data.get('status')
             reason = pokemon_data.get('reason', 'unknown reason')
--- 1_Exploring_Web_Technologies_and_Python_Programming_Task_3.py
+++ 1_Exploring_Web_Technologies_and_Python_Programming_Task_3.py
@@ -2,6 +2,7 @@ import requests
 
 #Task 3
 def fetch_pokemon_data(pokemon_name):
+    #connect and grab response from Pokeapi
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
     try:
         #
@@ -22,37 +23,36 @@ def fetch_pokemon_data(pokemon_name):
     
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
+    
+    finally:
+         return pokemon_data
 
 def calculate_average_weight(pokemon_list):
+    #initialize variables for use in calculation / output
     weight_list = []
     output = 'The average weight of '
     for pokemon in pokemon_list:
-        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
-        response = requests.get(url)
-        pokemon_data = response.json()
-        if isinstance(pokemon_data, dict):
+            pokemon_data = fetch_pokemon_data(pokemon)
             name = pokemon_data.get('name','No Name')
             weight = pokemon_data.get('weight','No Weight')
+            
+            #add pokemon names to output string
             if pokemon_list.index(pokemon) == 0:
                 output = output + name
             elif pokemon_list.index(pokemon) == (len(pokemon_list) - 1):
                 output = output + ', and ' + name
             else:
                 output = output + ', ' + name
+            
+            #add weight to list of weights
             weight_list.append(int(weight))
-        else:
-            status = pokemon_data.get('status')
-            reason = pokemon_data.get('reason', 'unknown reason')
-            print(f"Error {status}: {reason}")
     
+    #calculate average weight and add to output string in a formatted way
     average = sum(weight_list) / len(weight_list)
     output = output + f" is {average} hectograms or {average * 100} grams"
     print(output)
         
 pokemon_names = ['pikachu', 'bulbasaur', 'charmander']
-
-for pokemon in pokemon_names:
-    fetch_pokemon_data(pokemon)
 
 calculate_average_weight(pokemon_names)
 
